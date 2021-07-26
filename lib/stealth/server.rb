cbaffc48b488b5196e6e575f8acbe92866474eca
Bao Quoc <quoc@mmj.vn>
2021-07-26 17:01:45 +0700

    Add debugger

--- lib/stealth/server.rb
+++ lib/stealth/server.rb
@@ -31,7 +31,7 @@ module Stealth
 
     get_or_post '/incoming/:service' do
       Stealth::Logger.l(topic: params[:service], message: 'Received webhook.')
-      p request.body
+      p MultiJson.load(request.body.read)
       # JSON params need to be parsed and added to the params
       if request.env['CONTENT_TYPE']&.match(/application\/json/i)
         json_params = MultiJson.load(request.body.read)
